b0000c911fe16b2a48b29c4ac5cfa2525c3508b9
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-28 14:55:33 +0100

    Remove the setter and getter functions

--- app/staff.py
+++ app/staff.py
@@ -2,33 +2,24 @@ from app.person import Person
 
 
 class Staff(Person):
-    """ Class Staff; Acts as object class for Staff
-    Inherit the class Person
+    ''' Class Staff; Acts as object class for Staff
+
+    Inherits:
+        Person
+
     Instance Variable:
-    depertment - The department of the staff
-    methods:
-    get_department - returns the department of the staff
-    set_department - use to set the department variable
-    __init__
-    involves the constructor of Person class which it's
-    replacing
-    arguments:
-    name - Name of the staff
-    gender - the sex of the staff
-    department - department of the staff of the staff
-    """
+        depertment - The department of the staff
+
+    '''
+
     def __init__(self, name, gender, department="Unknown"):
+        '''involves the constructor of Person class which it's
+        replacing
+
+        Arguments:
+            name - Name of the staff
+            gender - the sex of the staff
+            department - department of the staff of the staff
+        '''
         self.department = department
-        #Person.__init__(name, gender)
         super().__init__(name, gender)
-
-    def get_department(self):
-        """ return the department of the staff """
-        return self.department
-
-    def set_department(self, department):
-        """ Set the department instance variable
-        arguments:
-        department - the department of the staff
-        """
-        self.department = department
--- test/test_staff.py
+++ test/test_staff.py
@@ -1,15 +1,15 @@
 import unittest
+
 from app.person import Person
 from app.staff import Staff
 
 
 class TestStaff(unittest.TestCase):
+    def setUp(self):
+        self.staff_obj = Staff("Joe Jack", "M", "Engineering")
 
-	def setUp(self):
-		self.staff_obj = Staff("Joe Jack", "M", "Engineering")
-	def test_person_inheritance(self):
-		self.assertTrue(issubclass(Staff, Person))
-	def test_get_dept(self):
-		self.assertEqual(self.staff_obj.get_department(), "Engineering")
-	def test_get_name(self):
-		self.assertEqual("Joe Jack", self.staff_obj.get_name())
+    def test_person_inheritance(self):
+        self.assertTrue(issubclass(Staff, Person))
+
+    def test_person_inheritance_two(self):
+        self.assertTrue(isinstance(self.staff_obj, Person))
