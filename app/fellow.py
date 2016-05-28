56a204123c166723481d9f93d1692b79042298a8
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-28 14:45:24 +0100

    remove the various helper and setter function

--- app/fellow.py
+++ app/fellow.py
@@ -6,51 +6,14 @@ class Fellow(Person):
     """ Hold the information of a fellow
     Acts as an object for Fellow. Inherit from Person
 
-    instance Variable:
-    level - developer level of fellow, D0, D1, D2..
-    stack - the stack the fellow works with
+    Methods:
+        want_accomodation - return the true/false base on fellow choice
 
-    methods:
-    set_level/get_level - setter and getter method for fellow's level
-    set_stack/get_stack - setter and getter method for fellow'd level
-    want_accomodation - return the true/false base on fellow choice
-    set_accomodation - setter fucntion for want accom
-
-    __init__
-    Initialize the Superclass constructor
-    arguments:
-    level
-    stack
-    want_accom
     """
 
-    def __init__(self, name, gender, want_accom="N", level="Unknown", stack="Unknown"):
+    def __init__(self, name, gender, want_accom="N"):
         super().__init__(name, gender)
-        self.level = level
-        self.stack = stack
         self.want_accom = want_accom
-
-    def set_level(self, level):
-        """ Set the level of fellow
-        arguments:
-        level
-        """
-        self.level = level
-
-    def set_stack(self, stack):
-        """ Set the stack of fellow
-        arguments
-        stack - the stack fellow is working
-        """
-        self.stack = stack
-
-    def get_level(self):
-        """ return the level fellow is """
-        return self.level
-
-    def get_stack(self):
-        """ return the stack of the fellow """
-        return self.stack
 
     def wants_accomodation(self):
         """ return value base on the value of want_accom"""
@@ -58,9 +21,3 @@ class Fellow(Person):
             return True
         else:
             return False
-
-    def set_wants_accom(self, want_accom):
-        """ set value for value for want_accom
-        arguments - want_accom
-        """
-        self.want_accom = want_accom
--- test/test_fellow.py
+++ test/test_fellow.py
@@ -6,32 +6,17 @@ from app.person import Person
 
 class FellowTest(unittest.TestCase):
     def setUp(self):
-        self.fellowA = Fellow("Malik Wahab", "M", "Y", "1", "Python")
+        self.fellowA = Fellow("Malik Wahab", "M", "Y")
         self.fellowB = Fellow("Jola Ade", "M")
 
     def test_person_inheritance(self):
         self.assertTrue(issubclass(Fellow, Person))
 
-    def test_set_level(self):
-        self.fellowB.set_level("1")
-        self.assertEqual(self.fellowB.level, "1")
-
-    def test_get_level(self):
-        self.assertEqual(self.fellowA.get_level(), "1")
-
-    def test_set_stack(self):
-        self.fellowB.set_stack("ruby")
-        self.assertEqual(self.fellowB.stack, "ruby")
-
-    def test_get_stack(self):
-        self.assertEqual(self.fellowA.get_stack(), "Python")
+    def test_person_inheritance_two(self):
+        self.assertTrue(isinstance(self.fellowA, Person))
 
     def test_wants_accomodation(self):
         self.assertTrue(self.fellowA.wants_accomodation())
 
     def test_wants_accomdation_two(self):
         self.assertFalse(self.fellowB.wants_accomodation())
-
-    def test_set_wants_accomdation(self):
-        self.fellowA.set_wants_accom("N")
-        self.assertFalse(self.fellowA.wants_accomodation())
