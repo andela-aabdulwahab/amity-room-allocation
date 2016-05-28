a94eb383170ed9a0c00a1b4f6e9933077c2421ba
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-28 17:08:37 +0100

    fix the docstring for livingroom

--- app/livingroom.py
+++ app/livingroom.py
@@ -5,26 +5,23 @@ from app.room import Room
 class LivingRoom(Room):
 
     """ object container for living room
-    inherit Room
+    Inherits
+        Room
 
-    instance variable
-    gender - gender of allowed occupants
+    Attributes:
+        gender(str): gender of allowed occupants
 
-    methods
-    get_gender - return the allowed occupants gender
-    add_occupant - override that of Room
-
-    __init__
-    set the size of the room to 4 by calling the base class
-    constructor to 4
-    set gender of allowed occupants
-
-    argument
-    name - name of the room
-    gener - the gender allowed for the room
     """
 
     def __init__(self, name, gender):
+        """set the size of the room to 4 by calling the base class
+        constructor to 4
+        set gender of allowed occupants
+
+        Arguments
+            name(str): name of the room
+            gener(str): the gender allowed for the room
+        """
         super().__init__(name, 4)
         self.gender = gender
 
@@ -32,13 +29,10 @@ class LivingRoom(Room):
         """ override the Room add_occupant
         determining if is a fellow or gender
 
-        arguments
-        person - instance person_object
+        Arguments:
+            person: instance person_object
 
-        return status code
-           1 - successful
-           5 - person is not a fellow
-           4 - Gender not in room
+        Returns:
         """
         if not isinstance(person_obj, Fellow):
             return 5
