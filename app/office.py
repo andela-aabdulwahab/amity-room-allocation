300520aeebc56bbc3adc4b62c12e8510cf77e189
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-28 16:49:38 +0100

    [feature] Added docstring to Office class

--- app/office.py
+++ app/office.py
@@ -2,6 +2,14 @@ from app.room import Room
 
 
 class Office(Room):
+    """ object container for living room
+    Inherits:
+        Rooms
+
+    """
 
     def __init__(self, name):
+        """Calls the room constructor with size equals 6
+
+        """
         super().__init__(name, 6)
