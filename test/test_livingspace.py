17a0e8438b0af842a80e276d8e94a2eb8e264f72
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-29 10:57:34 +0100

    [chore] Chance the name of test file for LivingSpace

---
+++ test/test_livingspace.py
@@ -0,0 +1,31 @@
+import unittest
+
+from app.exceptions import PersonNotFellowError
+from app.fellow import Fellow
+from app.livingspace import LivingSpace
+from app.room import Room
+from app.staff import Staff
+
+
+class TestLivingRoom(unittest.TestCase):
+    def setUp(self):
+        self.spata = LivingSpace("spata", "M")
+        self.personA = Fellow("Malik Wahab", "M", "Y")
+        self.personB = Fellow("Temi Tope", "F", "N")
+        self.personC = Staff("Joe Jack", "F", "Engineering")
+
+    def test_room_inheritance(self):
+        self.assertTrue(issubclass(LivingSpace, Room))
+
+    def test_init_name(self):
+        self.assertEqual("spata", self.spata.name)
+
+    def test_init_size(self):
+        self.assertEqual(4, self.spata.size)
+
+    def test_gender(self):
+        self.assertEqual("M", self.spata.gender)
+
+    def test_add_occupants(self):
+        with self.assertRaises(PersonNotFellowError):
+            self.spata.occupants = self.personC
