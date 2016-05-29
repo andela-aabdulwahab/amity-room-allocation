b5a7eb1a48e1a34ba6ed60fc62d048ef425bf297
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-29 12:15:43 +0100

    [Feature] Rewrote testcase for Amity

--- test/test_amity.py
+++ test/test_amity.py
@@ -1,14 +1,8 @@
 import unittest
-import os, sys, inspect
-
-currentdir = os.path.dirname(os.path.abspath(inspect. \
-    getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
 
 from app.amity import Amity
 from app.fellow import Fellow
-from app.livingroom import LivingRoom
+from app.livingspace import LivingSpace
 from app.office import Office
 from app.staff import Staff
 
@@ -16,80 +10,81 @@ from app.staff import Staff
 class TestAmity(unittest.TestCase):
 
     def setUp(self):
-        self.fellowA = Fellow("Malik Wahab", "M", "D1", "Python")
-        self.fellowB = Fellow("Ose Oko", "F", "D1", "Ruby")
-        self.staffA = Staff("Joe Jack", "M", "Training")
-        self.staffB = Staff("Nengi Adoki", "F", "Beliefs")
+        self.fellowA = Fellow("Malik Wahab", "M", "Y")
+        self.fellowB = Fellow("Ose Oko", "F", "N")
+        self.staffA = Staff("Joe Jack", "M")
+        self.staffB = Staff("Nengi Adoki", "F")
         self.officeA = Office("Moon")
         self.officeB = Office("Mecury")
-        self.livingA = LivingRoom("Spata", "M")
-        self.livingB = LivingRoom("Roses", "F")
+        self.livingA = LivingSpace("Spata", "M")
+        self.livingB = LivingSpace("Roses", "F")
         self.amity = Amity()
-        self.amity.add_room(self.livingB)
+        self.amity.rooms = self.livingB
 
     def test_init(self):
         amity = Amity()
         self.assertEqual(amity.rooms, {})
+
     def test_add_room(self):
-        self.amity.add_room(self.livingA)
+        self.amity.rooms = self.livingA
         self.assertIn("spata", self.amity.rooms)
 
-    def test_get_room(self):
-        self.assertEqual(self.amity.get_room("roses"), self.livingB)
+    def test_get_rooms_two(self):
+        self.assertEqual(self.amity.rooms.get('roses'), self.livingB)
 
     def test_get_rooms(self):
-        self.amity.add_room(self.livingA)
+        self.amity.rooms = self.livingA
         room_dict = {'roses': self.livingB, 'spata': self.livingA}
-        self.assertEqual(self.amity.get_rooms(), room_dict)
+        self.assertEqual(self.amity.rooms, room_dict)
 
-    def test_get_persons(self):
-        self.amity.add_person(self.fellowA)
-        self.amity.add_person(self.staffA)
-        self.amity.add_person(self.fellowB)
-        self.amity.add_person(self.staffB)
-        self.amity.add_room(self.officeA)
-        self.amity.add_room(self.officeB)
-        person_dict = {"malikwahab": self.fellowA, "oseoko": self.fellowB, "joejack": self.staffA, "nengiadoki": self.staffB}
-        self.assertEqual(person_dict, self.amity.get_persons())
+    def test_get_persons_two(self):
+        self.amity.persons = self.fellowA
+        self.amity.persons = self.staffA
+        self.amity.persons = self.fellowB
+        self.amity.persons = self.staffB
+        self.assertIn(self.staffB.identifier, self.amity.persons)
 
     def test_get_person(self):
         self.amity.add_person(self.fellowA)
-        self.assertEqual(self.amity.get_person('malikwahab'), self.fellowA)
+        self.assertEqual(self.amity.persons[self.fellowA.identifier],
+                         self.fellowA)
 
     def test_get_offices(self):
-        self.amity.add_room(self.officeA)
-        self.amity.add_room(self.officeB)
-        self.amity.add_room(self.livingA)
-        self.amity.add_room(self.livingB)
+        self.amity.rooms = self.officeA
+        self.amity.rooms = self.officeB
+        self.amity.rooms = self.livingA
+        self.amity.rooms = self.livingB
         office_dict = {"moon": self.officeA, "mecury": self.officeB}
         self.assertEqual(office_dict, self.amity.get_offices())
 
-    def test_get_livingrooms(self):
-        self.amity.add_room(self.officeA)
-        self.amity.add_room(self.officeB)
-        self.amity.add_room(self.livingA)
-        self.amity.add_room(self.livingB)
+    def test_get_livingspaces(self):
+        self.amity.rooms = self.officeA
+        self.amity.rooms = self.officeB
+        self.amity.rooms = self.livingA
+        self.amity.rooms = self.livingB
         living_dict = {"spata": self.livingA}
-        self.assertEqual(living_dict, self.amity.get_livingrooms("M"))
+        self.assertEqual(living_dict, self.amity.get_livingspaces("M"))
 
-    def test_get_all_livingrooms(self):
-        self.amity.add_room(self.officeA)
-        self.amity.add_room(self.officeB)
-        self.amity.add_room(self.livingA)
-        self.amity.add_room(self.livingB)
+    def test_get_all_livingspaces(self):
+        self.amity.rooms = self.officeA
+        self.amity.rooms = self.officeB
+        self.amity.rooms = self.livingA
+        self.amity.rooms = self.livingB
         living_dict = {"spata": self.livingA, "roses": self.livingB}
-        self.assertEqual(living_dict, self.amity.get_all_livingrooms())
+        self.assertEqual(living_dict, self.amity.get_all_livingspaces())
 
     def test_add_person(self):
-        person = self.fellowA
-        self.amity.add_person(person)
-        self.assertIn(person.get_id(), self.amity.persons)
+        self.amity.persons = self.fellowA
+        self.assertIn(self.fellowA.identifier, self.amity.persons)
 
     def test_room_type(self):
-        self.assertEqual(self.amity.room_type(self.livingA), 'livingroom')
+        self.assertEqual(self.amity.room_type(self.livingA), 'livingspace')
 
     def test_room_type_two(self):
         self.assertEqual(self.amity.room_type(self.officeA), 'office')
+
+    def test_room_type_thee(self):
+        self.assertIsNone(self.amity.room_type({}))
 
     def test_person_type(self):
         self.assertEqual(self.amity.person_type(self.fellowA), 'fellow')
@@ -97,3 +92,5 @@ class TestAmity(unittest.TestCase):
     def test_person_type_two(self):
         self.assertEqual(self.amity.person_type(self.staffB), 'staff')
 
+    def test_person_type_three(self):
+        self.assertIsNone(self.amity.person_type({}))
