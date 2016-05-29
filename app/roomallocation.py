5b4f01a8622a357899afaf43beaad2e4c293605b
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-05-29 12:49:27 +0100

    [chore] Fix docstring structure and lines too long

--- app/roomallocation.py
+++ app/roomallocation.py
@@ -1,19 +1,8 @@
 import random
-import os, sys, inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect. \
-    getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-
-from app.office import Office
-from app.fellow import Fellow
-from app.allocation_db import AllocationDb
 from app.amity import Amity
-from app.staff import Staff
-from app.livingroom import LivingRoom
-
+from app.fellow import Fellow
+from app.office import Office
 
 
 class RoomAllocation():
@@ -21,88 +10,74 @@ class RoomAllocation():
     allocation application. provides an API for all action
     that the application can perform
 
-    instance variable
-    amity - an instance of class Amity
-
-    methods
-    create_room - add room to the amity class i.e the application
-    create_person - add person the application by adding the amity object
-    select_random - return a random meeber of a dictionaty passed
-    allocate_office - allocate office to person
-    allocate_room - allocate room to person
-    rellocate_person - rellocate person to the specified room
-    get_unallocated - return all unallocated person
-    print_unallocated_to_file - print all unallocated person information to file
-    print_allocation_to_file - print rooms and persons allocated to them
-    print_person_to_file - extract neccessary person information and print it to file
-    print_room_to_file - print person of a given room to file
-    save to database - save all rooms and persons in amity to the specified database
-    load_from_database - loads all persons and rooms in database into Amity
-
-    __init__
-    creates an instance variable of type Amity with the
-    object passed to it
-
-    arguments
-    amity_obj - object of class Amity
+    Attributes:
+        amity - an instance of class Amity
 
     '''
 
     def __init__(self, amity_obj):
+        """ creates an instance variable of type Amity with the
+        object passed to it
+
+        Arguments
+            amity_obj: object of class Amity
+        """
         self.amity = amity_obj
 
     def create_room(self, room_obj):
-        ''' Create a room and add to Amity
+        """ Create a room and add to Amity
 
-        arguments
-        room_obj - room to be added to database
-        '''
+        Arguments
+            room_obj: room to be added to database
+        """
         self.amity.add_room(room_obj)
 
     def create_person(self, person_obj):
-        ''' Create a person and add to Amity
+        """ Create a person and add to Amity
 
-        arguments
-        person_obj - person to be added to database
-        '''
+        Arguments:
+            person_obj: person to be added to database
+        """
         person_id = person_obj.get_id()
         if not self.amity.get_person(person_id):
             self.amity.add_person(person_obj)
             office_status = self.allocate_office(person_id)
-            if isinstance(person_obj, Fellow) and person_obj.wants_accomodation():
+            if (isinstance(person_obj, Fellow) and
+               person_obj.wants_accomodation()):
                 livingroom_status = self.allocate_livingroom(person_id)
-                return {"office_status": office_status, "room_status": livingroom_status}
+                return {"office_status": office_status,
+                        "room_status": livingroom_status}
             return {"office_status": office_status}
 
-
     def select_random(self, adict):
-        ''' Return a random member of the database provided
-        arguments
-        adict - this is the dictionary where random member will be selected
+        """ Return a random member of the database provided
 
-        sample picks "1" from keys and return a list. reason for [0]
-
-        '''
-        #random_key = random.sample(adict.keys(), 1)[0]
+        Arguments:
+            adict: this is the dictionary where random member will be selected
+        """
         random_key = random.choice(list(adict.keys()))
         return adict[random_key]
 
     def allocate_office(self, person_id):
-        ''' allocate office to person of specified id
-        arguments
-        person_id - id of person object that will be allocated to an office
+        """ allocate office to person of specified id
 
-        returns the status of the allocation
-        '''
+        Arguments:
+            person_id: id of person object that will be allocated to an office
+
+        Returns:
+            the status of the allocation
+        """
         return self.allocate_room(person_id, self.amity.get_offices())
 
     def allocate_livingroom(self, person_id):
-        ''' allocate livingroom to person of specified id
-        arguments
-        person_id - id of person object that will be allocated to a livingroom
+        """ allocate livingroom to person of specified id
+        Arguments:
+            person_id - id of person object that will be allocated to a
+            livingroom
 
-        returns the status of the allocation
-        '''
+        Returns:
+            the status of the allocation
+        """
         person_obj = self.amity.get_person(person_id)
         if person_obj is None:
             return 14
@@ -111,17 +86,18 @@ class RoomAllocation():
         return self.allocate_room(person_id, gender_livingrooms)
 
     def allocate_room(self, person_id, room_dict):
-        ''' allocate a random room from the supplied room dictionary
+        """ allocate a random room from the supplied room dictionary
         to person of specified id
 
-        arguments
-        person_id - id pf person object to be allocated to a random room
-        room_dict - a dictionary object of type room
+        Arguments
+            person_id: id pf person object to be allocated to a random room
+            room_dict: a dictionary object of type room
 
-        return 1 if successfull or error code if otherwise
-        '''
+        Return:
+
+        """
         person_obj = self.amity.get_person(person_id)
-        no_of_rooms = room_dict.keys().__len__()
+        no_of_rooms = len(room_dict.keys())
         if no_of_rooms < 1:
             return 14
         i = 0
@@ -147,13 +123,13 @@ class RoomAllocation():
                 return status
 
     def rellocate_person(self, person_id, new_room_id):
-        ''' rellocate person to the room specified
+        """ rellocate person to the room specified
 
-        arguments
-        person_id - Id of person to be rellocated
-        new_room_id - room to be relocated to
+        Arguments
+            person_id: Id of person to be rellocated
+            new_room_id: room to be relocated to
 
-        '''
+        """
         person_obj = self.amity.get_person(person_id)
         if person_obj is None:
             return 14
@@ -173,14 +149,14 @@ class RoomAllocation():
         else:
             return 7
 
-
     def remove_room(self, room_id):
-        ''' Remove a specified room from amity
+        """ Remove a specified room from amity
         render all person in room unallocated
 
-        arguments
-        room_id - id of room to be remove from amity
-        '''
+        Arguments
+            room_id - id of room to be remove from amity
+
+        """
         room = self.amity.get_room(room_id)
         if room is None:
             return 14
@@ -193,14 +169,13 @@ class RoomAllocation():
             del self.amity.rooms[room_id]
         return self
 
-
     def remove_person(self, person_id):
-        ''' Remove a specified person from amity
+        """ Remove a specified person from amity
         remove person form room occupied
 
-        arguments
-        person_id - id of the person to be remove
-        '''
+        Arguments:
+            person_id: id of the person to be remove
+        """
         person = self.amity.get_person(person_id)
         if person is None:
             return 14
@@ -216,18 +191,20 @@ class RoomAllocation():
         return self
 
     def get_unallocated(self):
-        ''' gets all person in amity without an office
+        """ gets all person in amity without an office
         or a livingroom
 
-        return
-        a list of dictionary of persons unallocated for livingroom and office
-        '''
+        Returns:
+            list: a list of dictionary of persons unallocated for livingroom
+            and office
+        """
         persons = self.amity.get_persons()
         office_unallocated = {}
         livingroom_unallocated = {}
         for person_id in persons:
             if isinstance(persons[person_id], Fellow):
-                if not persons[person_id].is_allocated("office") and not persons[person_id].is_allocated("livingroom"):
+                if not persons[person_id].is_allocated("office") and \
+                   not persons[person_id].is_allocated("livingroom"):
                     office_unallocated[person_id] = persons[person_id]
                     livingroom_unallocated[person_id] = persons[person_id]
                 elif not persons[person_id].is_allocated("office"):
@@ -240,52 +217,55 @@ class RoomAllocation():
         return [office_unallocated, livingroom_unallocated]
 
     def print_unallocated_to_file(self, file_name):
-        ''' Prints name of unallocated person to file specified
+        """ Prints name of unallocated person to file specified
 
-        arguments
-        file_name - path and name to file where information will be printed
-        '''
+        Arguments:
+            file_name: path and name to file where information will be printed
+        """
         file_obj = open(file_name, "w")
         unallocated_string = self.build_unallocation_string()
         file_obj.write(unallocated_string)
         file_obj.close()
 
     def build_unallocation_string(self):
-        ''' Builds the string of unallocated persons
+        """ Builds the string of unallocated persons
 
-        return
-        string of unallocated persons
-        '''
+        Return
+            string of unallocated persons
+
+        """
         unallocated = self.get_unallocated()
         office_unallocated = unallocated[0]
         livingroom_unallocated = unallocated[1]
         unallocated_string = " "
         unallocated_string += " --Unallocated for Office-- \n\n"
         for person_id in office_unallocated:
-            unallocated_string += self.print_person(office_unallocated[person_id])
+            unallocated_string += \
+                self.print_person(office_unallocated[person_id])
         unallocated_string += "\n \n --Unallocated for LivingRoom-- \n\n"
         for person_id in livingroom_unallocated:
-            unallocated_string += self.print_person(livingroom_unallocated[person_id])
+            unallocated_string += \
+                self.print_person(livingroom_unallocated[person_id])
         return unallocated_string
 
     def print_allocation_to_file(self, file_name):
-        ''' Prints the room allocation at Amity to file
+        """ Prints the room allocation at Amity to file
 
-        arguments
-        file_name - path and name to file where information will be printed
-        '''
+        Arguments:
+            file_name: path and name to file where information will be printed
+        """
         file_obj = open(file_name, "w")
         allocation_string = self.build_allocation_string()
         file_obj.write(allocation_string)
         file_obj.close()
 
     def build_allocation_string(self):
-        ''' BUild the string of the allocation in amity with rooms
+        """ BUild the string of the allocation in amity with rooms
         and persons in rooms
 
-        return
-        a string of the allocation
-        '''
+        Return
+            a string of the allocation
+        """
         rooms = self.amity.get_rooms()
         allocation_string = ""
         for i in rooms:
@@ -294,33 +274,33 @@ class RoomAllocation():
         return allocation_string
 
     def print_room(self, room_id):
-        ''' Builds the string of a room with the persons in it
+        """ Builds the string of a room with the persons in it
 
-        arguments
-        room_id - id of room to print
+        Arguments:
+            room_id - id of room to print
 
-        '''
+        """
         room = self.amity.get_room(room_id)
         room_string = ""
         if isinstance(room, Office):
                 room_type = "Office"
         else:
             room_type = "Living Room"
-        room_string += "\n--"+room.get_name()+"("+room_type+")--\n"
+        room_string += "\n--" + room.get_name() + "(" + room_type + ")--\n"
         persons = room.get_occupants()
         for i in persons:
             room_string += self.print_person(persons[i])
         return room_string
 
     def print_person(self, person_obj):
-        ''' Builds a string of person object printing out the name
+        """ Builds a string of person object printing out the name
 
-        arguments
-        person_obj - person to printed to file
+        Arguments:
+            person_obj - person to printed to file
 
-        returns
-        a string of the persons name, person_type and accomodation status
-        '''
+        Returns:
+            string: of the persons name, person_type and accomodation status
+        """
         name = person_obj.get_name().upper()
         want_accomodation = ""
         if isinstance(person_obj, Fellow):
@@ -331,12 +311,12 @@ class RoomAllocation():
         return name + ' ' + person_type + ' ' + want_accomodation + "\n"
 
     def save_to_database(self, allocation_db_obj):
-        ''' Save the current state of person to database, saving all
+        """ Save the current state of person to database, saving all
         persons and rooms in Amity to database
 
-        argument
-        allocation_db_obj - database connection to be use to save data
-        '''
+        Argument:
+            allocation_db_obj - database connection to be use to save data
+        """
         allocation_db_obj.empty_tables()
         persons = self.amity.get_persons()
         rooms = self.amity.get_rooms()
@@ -347,12 +327,12 @@ class RoomAllocation():
         return self
 
     def load_from_database(self, allocation_db_obj):
-        ''' load database information to create a new application state
+        """ load database information to create a new application state
         reset Amity to the new state
 
         argument
-        allocation_db_obj - database connection to be use to save data
-        '''
+            allocation_db_obj - database connection to be use to save data
+        """
         persons = allocation_db_obj.get_persons()
         rooms = allocation_db_obj.get_rooms()
         amity = Amity()
