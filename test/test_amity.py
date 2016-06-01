7eecaa366ed3da6ff00b88dfdaa3fca0def6908f
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-06-01 12:39:02 +0100

    [feature] allow current directory detecting

--- test/test_amity.py
+++ test/test_amity.py
@@ -1,4 +1,10 @@
 import unittest
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.
+    getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 
 from app.amity import Amity
 from app.exceptions import SameNameRoomError
