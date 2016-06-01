71a561f9617373616a0d5f29f9629fb92fa76d8f
malikwahab <abdulmalik.abdulwahab@andela.com>
2016-06-01 14:18:52 +0100

    [feature] refactor test_app

--- test/test_app.py
+++ test/test_app.py
@@ -30,14 +30,9 @@ class Capturing(list):
 class TestApp(unittest.TestCase):
 
     def setUp(self):
+        self.reset_call()
         amity = Amity()
         self.roomallocation = RoomAllocation(amity)
-        self.args = {'create_room': False, 'add_person': False,
-                     'relocate_person': False, 'print_allocation': False,
-                     'print_unallocated': False, 'print_room': False,
-                     'load_state': False, 'print_room': False,
-                     'save_state': False, 'delete_room': False,
-                     'delete_person': False, 'print_persons': False}
         self.args['<room_name>'] = ['Mars', 'Sapele']
         self.args['<room_type>'] = ['office', 'livingspace']
         self.args['--rgender'] = ['M']
@@ -50,15 +45,44 @@ class TestApp(unittest.TestCase):
         self.args["--wants_accom"] = 'Y'
         self.args['add_person'] = True
         main(self.args)
+        self.reset_call()
 
 
     def tearDown(self):
         if os.path.isfile('roomallocation.p'):
             os.remove('roomallocation.p')
 
+    def create_person(self, person_type):
+        self.args['<first_name>'] = 'Jide'
+        self.args['<last_name>'] = 'Olu'
+        self.args['<gender>'] = 'M'
+        self.args['<person_type>'] = person_type
+        self.args["--wants_accom"] = 'Y'
+        self.args['add_person'] = True
+
+    def create_room(self, room_type, gender =[]):
+        self.args['<room_name>'] = ['Mecury']
+        self.args['<room_type>'] = [room_type]
+        self.args['--rgender'] = gender
+        self.args['create_room'] = True
+
+    def capture_output(self):
+        with Capturing() as output:
+            main(self.args)
+        return '\n'.join(output)
+
+    def relocate_person(self, identifier):
+        self.args['relocate_person'] = True
+        self.args['<person_identifier>'] = identifier
+        self.args['<new_room_name>'] = 'mecury'
+
     def reset_call(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.args = {'create_room': False, 'add_person': False,
+                     'relocate_person': False, 'print_allocation': False,
+                     'print_unallocated': False, 'print_room': False,
+                     'load_state': False, 'print_room': False,
+                     'save_state': False, 'delete_room': False,
+                     'delete_person': False, 'print_persons': False}
 
     def test_load_state(self):
         roomallocation = load_state()
@@ -69,153 +93,87 @@ class TestApp(unittest.TestCase):
         self.assertTrue(os.path.isfile('roomallocation.p'))
 
     def test_add_person(self):
-        self.args['<first_name>'] = 'Jide'
-        self.args['<last_name>'] = 'Olu'
-        self.args['<gender>'] = 'M'
-        self.args['<person_type>'] = 'fellow'
-        self.args["--wants_accom"] = 'Y'
-        self.args['add_person'] = True
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.create_person('fellow')
         expected_string = "Jide allocated to Office mars\n"
         expected_string += "Jide allocated to Living Space sapele\n"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_add_person_staff(self):
-        self.args['<first_name>'] = 'Malik'
-        self.args['<last_name>'] = 'Wahab'
-        self.args['<gender>'] = 'M'
-        self.args['<person_type>'] = 'staff'
-        self.args["--wants_accom"] = 'Y'
-        self.args['add_person'] = True
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
-        expected_string = "Malik allocated to Office mars\n"
-        self.assertEqual(output_string, expected_string)
+        self.create_person('staff')
+        expected_string = "Jide allocated to Office mars\n"
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_add_person_three(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+
         self.args['delete_room'] = True
         self.args['<room_name>'] = ['mars']
         main(self.args)
         self.args['<room_name>'] = ['sapele']
         main(self.args)
-        self.args['<first_name>'] = 'Malik'
-        self.args['<last_name>'] = 'Wahab'
-        self.args['<gender>'] = 'M'
-        self.args['<person_type>'] = 'Fellow'
-        self.args["--wants_accom"] = 'Y'
-        self.args['add_person'] = True
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
-        expected_string = "No available Office to allocate Malik\n"
-        expected_string += "No available Living Space to allocate Malik\n"
-        self.assertEqual(output_string, expected_string)
+        self.create_person('fellow')
+        expected_string = "No available Office to allocate Jide\n"
+        expected_string += "No available Living Space to allocate Jide\n"
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_add_person_invalid(self):
-        self.args['<first_name>'] = 'Malik'
-        self.args['<last_name>'] = 'Wahab'
-        self.args['<gender>'] = 'M'
-        self.args['<person_type>'] = 'invalid'
-        self.args["--wants_accom"] = 'Y'
-        self.args['add_person'] = True
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.create_person('invalid')
         expected_string = "Invalid type: person can be fellow or staff"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_create_room(self):
-        self.args['<room_name>'] = ['Mecury']
-        self.args['<room_type>'] = ['office']
-        self.args['--rgender'] = []
-        self.args['create_room'] = True
-        self.args['add_person'] = False
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.create_room('office')
         expected_string = "Room(s) created with Id (type): \n"
         expected_string += "mecury (office) \n"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_create_room_exist(self):
+        self.reset_call()
         self.args['<room_name>'] = ['Mars']
         self.args['<room_type>'] = ['Office']
         self.args['--rgender'] = ['']
         self.args['create_room'] = True
-        self.args['add_person'] = False
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Room(s) created with Id (type): \n"
         expected_string += "[Error] Room with name Mars already exit in Amity"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_create_room_invalid(self):
-        self.args['<room_name>'] = ['Mars']
-        self.args['<room_type>'] = ['invalid']
-        self.args['--rgender'] = ['']
-        self.args['create_room'] = True
-        self.args['add_person'] = False
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.create_room('invalid')
         expected_string = "Room(s) created with Id (type): \n"
-        expected_string += "Invalid room type for [Mars invalid]\n"
-        self.assertEqual(output_string, expected_string)
+        expected_string += "Invalid room type for [Mecury invalid]\n"
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_relocate_room(self):
-        self.args['<room_name>'] = ['Mecury']
-        self.args['<room_type>'] = ['office']
-        self.args['--rgender'] = []
-        self.args['create_room'] = True
-        self.args['add_person'] = False
+        self.reset_call()
+        self.create_room('office')
         main(self.args)
         roomallocation = load_state()
         for i in roomallocation.amity.persons:
             person = roomallocation.amity.persons[i]
-        self.args['relocate_person'] = True
-        self.args['create_room'] = False
-        self.args['add_person'] = False
-        self.args['<person_identifier>'] = person.identifier
-        self.args['<new_room_name>'] = 'mecury'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.relocate_person(person.identifier)
         expected_string = "Person relocated to mecury"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_relocate_room_two(self):
-        self.args['relocate_person'] = True
-        self.args['create_room'] = False
-        self.args['add_person'] = False
-        self.args['<person_identifier>'] = 'malikwahab'
-        self.args['<new_room_name>'] = 'mecury'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
+        self.relocate_person('invalid')
         expected_string = "Invalid Id supplied"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_print_allocation(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_allocation'] = True
         self.args['--option'] = ''
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         roomallocation = load_state()
         expected_string = roomallocation.build_allocation_string()
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_print_allocation_file(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_allocation'] = True
         self.args['--option'] = 'test/test_print_allocation.txt'
         main(self.args)
@@ -224,8 +182,7 @@ class TestApp(unittest.TestCase):
         self.assertTrue(file_created)
 
     def test_print_unallocated(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_unallocated'] = True
         self.args['--option'] = ''
         with Capturing() as output:
@@ -233,11 +190,10 @@ class TestApp(unittest.TestCase):
         output_string = '\n'.join(output)
         roomallocation = load_state()
         expected_string = roomallocation.build_unallocation_string()
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_print_unallocated_file(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_unallocated'] = True
         self.args['--option'] = 'test/test_print_unallocated.txt'
         main(self.args)
@@ -246,125 +202,82 @@ class TestApp(unittest.TestCase):
         self.assertTrue(file_created)
 
     def test_print_room(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_room'] = True
         self.args['<room_name>'] = ['mars']
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         roomallocation = load_state()
         expected_string = roomallocation.print_room('mars')
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_print_room_invalid(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_room'] = True
         self.args['<room_name>'] = ['invalid']
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Invalid room id supplied"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def print_persons(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['print_persons'] = True
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         roomallocation = load_state()
         expected_string = roomallocation.print_persons()
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_delete_person(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['delete_person'] = True
         roomallocation = load_state()
         for i in roomallocation.amity.persons:
             person = roomallocation.amity.persons[i]
         self.args['<person_id>'] = person.identifier
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = person.identifier + " deleted from Amity"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_delete_person_invalid(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['delete_person'] = True
         self.args['<person_id>'] = 'invalid'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Invalid Id supplied"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_delete_room(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['delete_room'] = True
         self.args['<room_name>'] = ['mars']
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "mars deleted from Amity"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_delete_room_invalid(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['delete_room'] = True
         self.args['<room_name>'] = 'invalid'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Invalid Id supplied"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_save_state_db(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['save_state'] = True
         self.args["--db"] = ''
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Data Successfully saved to database"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_save_state_db_two(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['save_state'] = True
         self.args["--db"] = 'test/roomallocation.db'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Data Successfully saved to database"
+        output_string = self.capture_output()
         os.remove('test/roomallocation.db')
         self.assertEqual(output_string, expected_string)
 
     def test_load_state_db(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
+        self.reset_call()
         self.args['load_state'] = True
         self.args["<db_path>"] = 'roomallocation.db'
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
         expected_string = "Application State reload from database"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
 
     def test_main_no_args(self):
-        self.args['create_room'] = False
-        self.args['add_person'] = False
-        with Capturing() as output:
-            main(self.args)
-        output_string = '\n'.join(output)
+        self.reset_call()
         expected_string = "use the usage instruction below to build command"
-        self.assertEqual(output_string, expected_string)
+        self.assertEqual(self.capture_output(), expected_string)
